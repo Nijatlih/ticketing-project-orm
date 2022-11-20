050949e9a34f1b84692b65b5456e13cd59333baf
nijat <nijat.de@gmail.com>
2022-11-20 12:32:21 +0100

    Part-4 Task Service and impl

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -73,4 +73,14 @@ public class TaskServiceImpl implements TaskService {
         }
     }
 
+    @Override
+    public int totalNonCompletedTask(String projectCode) {
+        return taskRepository.totalNonCompletedTasks(projectCode);
+    }
+
+    @Override
+    public int totalCompletedTask(String projectCode) {
+        return taskRepository.totalCompletedTasks(projectCode);
+    }
+
 }
